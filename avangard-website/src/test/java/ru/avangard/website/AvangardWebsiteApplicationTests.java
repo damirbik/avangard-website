790bbc8dc4ee46fd2d9bfeeb1d707d8860c50935
damirbik <159629681+damirbik@users.noreply.github.com>
2025-10-23 20:19:38 +0300

    Add files via upload

---
+++ avangard-website/src/test/java/ru/avangard/website/AvangardWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.avangard.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvangardWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
