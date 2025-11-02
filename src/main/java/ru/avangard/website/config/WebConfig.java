71ab4370024e358d364198bd61df45e3b446798a
damirbik <dbikchurayev@bk.ru>
2025-11-02 20:51:19 +0300

    images fix

--- src/main/java/ru/avangard/website/config/WebConfig.java
+++ src/main/java/ru/avangard/website/config/WebConfig.java
@@ -6,10 +6,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:uploads/");
+                .addResourceLocations("file:uploads/images/");
     }
 }
